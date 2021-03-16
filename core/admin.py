9724338a1b1d799297e02efb88ecd0132f887df9
Aitsam <67542105+aitsam12@users.noreply.github.com>
2021-03-16 13:35:34 +0100

    Basic code of my Django profile

---
+++ core/admin.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+
+from .views import *
+
+admin.site.register(About)
+admin.site.register(Education)
+admin.site.register(Experience)
+admin.site.register(Skills)
+admin.site.register(MyProjects)
+admin.site.register(Publications)
+admin.site.register(Achievements)
